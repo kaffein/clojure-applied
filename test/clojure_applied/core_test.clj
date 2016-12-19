cfe8b567eadf136e4de34e569b35a500dbc9b398
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2016-12-20 00:14:09 +0100

    Add default generated core file and related tests

---
+++ test/clojure_applied/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-applied.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-applied.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
