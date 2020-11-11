afa2c7df58afdca56baeb8f4c37d6d3bc2efc5f0
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2020-11-11 22:13:40 +0100

    Illustrate the use of agents for atomic asynchronous changes

--- src/ch5/stats.clj
+++ src/ch5/stats.clj
@@ -56,3 +56,92 @@
 (let [f (future-call terminate-john-connor)]
   (when (not (future-done? f))
     (future-cancel f)))
+
+
+
+;; ASYNCHRONOUS AND STATEFUL
+;; So far, we have mainly talked about 3 kinds of state containers in Clojure that are
+;; updated using the Unified Update Model :
+;; - vars
+;; - atoms
+;; - refs
+;; But there is a fourth one that we have omitted so far : `agent`s.
+
+;; While an atom is a state container which is to be updated atomically and synchronously
+;; and that refs are used when there needs to be coordination regarding the synchronous
+;; update of multiple state containers, `agent`s on the other hand allow single shared state
+;; `asynchronous` update.
+;; Modifying the state of an agent is done via an `action function` which is merely a
+;; function that is applied to the current state of the agent with some optional arguments
+;; giving the new state of the agent. We retrieve here the same pattern that we saw with
+;; the Unified Update Model. The application of this function is done `asynchronously` and
+;; ordering the application of the function is called a `dispatch`.
+
+;; There are two ways (functions) for dispatching an action :
+;; - send : used for CPU-limited actions (non blocking). The underlying thread-pool uses
+;; a fixed number of threads and thus rely on the completion of the actions in a timely
+;; manner
+;; - send-off : used for potentially-blocking actions (network call, I/O etc.). The 
+;; underlying thread-pool used here will grow on demand, so potentially blocking operations
+;; is not an issue.
+
+;; Let's take the example of a terminator to which we would like to send kill orders.
+;; Instead of going back and forth in the time machine (because it is expensive as sh**)
+;; let's say that the terminator will only start terminating its target when it has
+;; 3 people from the resistance in its kill-list.
+
+;; we model the terminator as an agent (kinda makes sense here)
+(def terminator (agent #{}))
+
+;; in order for it to proceed to its doings, we need a way to check whether the kill-list
+;; has at least 3 person from the resistance. Let's add a watch function that would allow to
+;; check that fact. A little reminder, a watch-fn associated with a state container is called
+;; each time the value of that state container changes.
+
+(defn put-terminator-in-time-machine
+  "This is just a helper function for fun"
+  [kill-list]
+  (let [target-list (clojure.string/join ", " (map :surname kill-list))]
+    (println "I have to terminate : " target-list)
+    (println "I'll be back!!!")))
+
+(add-watch terminator :kill-list-count
+           (fn [_ _ _ new-value]
+             (if (= 3 (count new-value))
+               (put-terminator-in-time-machine new-value))))
+
+;; now that we have a way to trigger the sending of our terminator, we now can focus on
+;; providing it with a list of target to be terminated.
+(send-off terminator conj {:name "Connor" :surname "John"})
+
+;; at this point, we have inserted John as the first target to be terminated. To make sure
+;; that the order has been ack'ed by our killer robot, we just use the same old trick as for
+;; the other reference types to check its content, which is deref-ing the agent.
+@terminator
+;; > #{{:name "Connor", :surname "John"}} and we indeed have John in the kill-list
+
+;; let's add two more targets to the list :
+(send-off terminator conj {:name "Reeves" :surname "Kyle"})
+(send-off terminator conj {:name "Connor" :surname "Sarah"})
+
+;; as soon as our kill-list have 3 targets in it, the watch that we have registered earlier
+;; will trigger the sending of our killer robot ...
+;; and indeed from the repl output we have :
+;; > I have to terminate :  Sarah, Kyle, John
+;; > I'll be back!!!
+
+;; To sum up on how `agent`s work. When a `dispatch` is made via `send` or `send-off` :
+;; - the call returns immediately
+;; - at some point later, in another thread, the new value of the agent is computed by
+;; applying the `action function` with the current state of the agent and any eventual
+;; arguments passed to it via the `dispatch`
+;; - if a `validator` function has been attached to the agent, it is triggered and
+;; provided with the newly processed value. If the new value passed validation, it
+;; is committed and becomes the new value of the agent
+;; - at this point, if one or more `watch` functions has been attached to the agent
+;; they are also triggered as soon as the new value is committed.
+
+;; Finally, some details worth mentionning regarding errors and exceptions in agents:
+;; when an exception or an error occurs on a agent, any subsequent interactions with
+;; this agent will throw an exception. It is then necessary to restart the agent with
+;; the `restart-agent` function.
