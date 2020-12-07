4222deb1ebf0df00aa4efebd46bc9586e1ffdcea
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2020-12-07 17:34:12 +0100

    Add core.async as dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [prismatic/schema "1.1.7"]
-                 [medley "1.0.0"]])
+                 [medley "1.0.0"]
+                 [org.clojure/core.async "1.3.610"]])
