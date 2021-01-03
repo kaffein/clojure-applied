f1bc2cbdc61c908aaf84b4a60037d660a233ab54
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2021-01-03 19:21:33 +0100

    Introduce core.async one-to-many channel abstractions
     - Explain the available abstractions for : fan-out and fan-in
     - Enumerate the different constructs for fan-out
     - Illustrate the use of `split` to divide the traffic arriving
    on a split channel in 2 based on the application of a predicate on
    each message values

--- src/ch6/core_async.clj
+++ src/ch6/core_async.clj
@@ -1,6 +1,6 @@
 (ns ch6.core-async
   (require [clojure.core.async :as async
-            :refer [>! <!]]))
+            :refer [>! <! >!! <!!]]))
 
 ;; After seeing how we can interact with components either directly or asynchronously
 ;; through their api, we are now going to see how we can establish more enduring
@@ -104,5 +104,89 @@
 
 ;; > REPL OUTPUT
 ;; ["SARAH CONNOR" "JOHN CONNOR" "KYLE REEVES"]
+
+
+;; FAN OUT (ONE TO MANY)
+;; core.async provides multiple abstractions for publishing messages on a channel
+;; to many consumers. This allows multiple independent consumers to process messages
+;; for different purposes.
+;; There are 3 main constructs provided by the library :
+;; - split : an abstraction allowing messages to be published on two different
+;; channels based on a `predicate` function, a fn applied to each message allowing
+;; it be put on either one of the channels depending on if the value of the message
+;; satisfies the predicate or not.
+;; - mult : an abstracton returning a multiple of the supplied channel. Channels
+;; containing copies of the content of the supplied channel can be added to the
+;; mult with `tap` and remove with `untap` : those channels will all
+;; receive any item published to the mult. Since the distribution is synchronous and
+;; in parallel, a buffering policy has to be set in order to avoid congestion if any of
+;; the tap is not capable of keeping up with the pace when processing items.
+;; - pub/sub : an abstraction like `mult` but allowing multiple consumers to `specialize`
+;; by consuming messages on specialized partitions of the supplied channel called
+;; `topic`s. Each topic corresponds to a certain type of message and unlike `mult`,
+;; instead of all consumers receiving the same message, each consumer can now
+;; explicitly express their intent to only be concerned with a certain type of
+;; message published by the `pub` by `sub`-scribing to a topic.
+
+;; Let's for e.g define a channel
+(def detection-input-chan (async/chan (async/dropping-buffer 5)))
+
+(def main-characters
+  [{:firstname "John"
+    :lastname "Connor"
+    :type :human}
+   {:model :t1000
+    :type :terminator}
+   {:firstname "Sarah"
+    :lastname "Connor"
+    :type :human}
+   {:firstname "Kyle"
+    :lastname "Reeves"
+    :type :human}
+   {:model :tx
+    :type :terminator}])
+
+;; Let's use a `split` to separate the humans from the machines. We then need
+;; to provide a predicate to the split. This predicate will return true if the
+;; entity is a :human and false otherwise (including when it is a :terminator).
+
+(defn human?
+  [entity]
+  (= :human (:type entity)))
+
+(defn split-channel
+  "Splits a channel based on whether an entity is of :type :human or :terminator
+  and returns the two resulting channels corresponding to each type in a vector"
+  [input-chan]
+  (async/split human? input-chan (async/dropping-buffer 1) (async/dropping-buffer 1)))
+
+;; We now need to set up the input channel `split`-ing and the receiving processes
+(let [[human-chan machine-chan] (split-channel detection-input-chan)]
+  (async/go-loop []
+    (println "Human identified : " (:firstname (<! human-chan)))
+    (recur))
+
+  (async/go-loop []
+    (println "Intruder detected : " (:model (<! machine-chan)))
+    (recur)))
+
+;; And then finally, we need to feed the values to the processes via the split channel
+(async/go
+  (doseq [mc main-characters]
+    (println "Sending : " mc)
+    (>! detection-input-chan mc)))
+
+;; > REPL OUTPUT
+;; Sending :  {:firstname John, :lastname Connor, :type :human}
+;; Sending :  {:model :t1000, :type :terminator}
+;; Human identified :  John
+;; Sending :  {:firstname Sarah, :lastname Connor, :type :human}
+;; Intruder detected :  :t1000
+;; Sending :  {:firstname Kyle, :lastname Reeves, :type :human}
+;; Human identified :  Sarah
+;; Sending :  {:model :tx, :type :terminator}
+;; Human identified :  Kyle
+;; Intruder detected : (model)  :tx
+;; user>
 
 
